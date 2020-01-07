404a314177c02e4b69bc7d9eb3b701723aaefd7f
SachinSaini499 <sachinsaini499@gmail.com>
2020-01-07 17:04:56 +0530

    Your Message

---
+++ BASS_Sample.UITest/AppInitializer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+namespace BASS_Sample.UITest
+{
+    public class AppInitializer
+    {
+        public static IApp StartApp(Platform platform)
+        {
+            if (platform == Platform.Android)
+            {
+                return ConfigureApp.Android.InstalledApp("com.companyname.bass_sample").StartApp();
+            }
+            return ConfigureApp.iOS.StartApp();
+        }
+    }
+}
